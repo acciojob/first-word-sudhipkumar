5802a44cf835d12f6523681f9538d6e8d745b51a
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-27 20:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ function firstWord(s) {
 }
 
 // Do not change the code below
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
